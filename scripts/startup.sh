385f78e37ecc3c07936043b983a014a491133050
maki <sunwood.ai.labs@gmail.com>
2025-09-24 12:13:59 +0000

    🔧 btopインストールコマンドからsudoを除去

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -4,4 +4,8 @@ set -euo pipefail
 apt-get update -y
 apt-get install -y google-cloud-sdk
 apt-get install -y snapd
-sudo snap install btop
+snap install btop
+
+curl -fsSL https://raw.githubusercontent.com/Sunwood-ai-labs/AMATERASU/refs/heads/main/scripts/docker-compose_setup_script.sh | sudo bash -s --
+
+curl -fsSL https://raw.githubusercontent.com/Sunwood-ai-labsII/baby-monitor-web-app/refs/heads/develop/scripts/create-swap-10G.sh | sudo bash -s --
